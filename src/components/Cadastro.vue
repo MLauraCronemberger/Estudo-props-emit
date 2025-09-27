653aee6e595f4561285aa101476ae18389997cc0
MLauraCronemberger <maria.laura.cronemberger@gmail.com>
2025-09-26 23:28:05 -0300

    Modal de Cadastro finalizado

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -1,33 +1,62 @@
 <template>
+  <v-dialog
+    v-model="mostrarModal"
+    max-width="600"
+    transition="dialog-bottom-transition"
+    eager
+  >
+    <v-card>
+      <v-card-text>
+        <v-text-field
+          label="Série"
+          required
+          outlined
+          v-model="objeto.serie"
+        />
+        <v-text-field
+          label="Nível Ensino"
+          required
+          outlined
+          v-model="objeto.nivelEnsino"
+        />
+        <v-text-field
+          label="Turma"
+          required
+          outlined
+          v-model="objeto.turma"
+        />
+        <v-text-field
+          label="Turno"
+          required
+          outlined
+          v-model="objeto.turno"
+        />
+      </v-card-text>
 
-<v-text-field
-    label="Nome"
-    required
-    outlined
-    width="500px"
-    v-model="objeto.nome"
-    >
-
-</v-text-field>
-
-<v-button
-label="cadastrar">
-
-</v-button>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="primary" @click="salvar">Cadastrar</v-btn>
+        <v-btn text @click="mostrarModal = false">Fechar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const objeto = {
-    nome: ""
+const objeto = ref({
+  serie: '',
+  nivelEnsino: '',
+  turma: '',
+  turno: '',
+})
 
+const mostrarModal = ref(true) // << ref aqui!
+
+function salvar() {
+  console.log(objeto.value)
+  mostrarModal.value = false
 }
-
-
 </script>
-
-<style>
-
-</style>
